9dfe5452be250708d2f0a75e73bd407370adc425
Ajetech1 <jeleelajetunmobi@gmail.com>
2024-06-19 05:09:26 +0100

    Navbar css updated

--- src/Components/TopNavbar.jsx
+++ src/Components/TopNavbar.jsx
@@ -1,7 +1,9 @@
 import React from "react";
-import { Navbar, Nav, Container } from "react-bootstrap";
+import { Navbar, Nav, Container, Form, FormControl } from "react-bootstrap";
 import { Link } from "react-router-dom";
+import { FaSearch } from "react-icons/fa";
 import Logo from "../assets/images/innkeeper.png";
+import "../assets/css/TopNavbar.css";
 
 const TopNavbar = () => {
   return (
@@ -12,10 +14,16 @@ const TopNavbar = () => {
         </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
+          <Form className="d-flex ms-auto me-3">
+            <FormControl
+              type="search"
+              placeholder="Search for courses"
+              className="me-2"
+              aria-label="Search"
+            />
+            <FaSearch className="search-icon" />
+          </Form>
           <Nav className="ms-auto">
-            <Nav.Link as={Link} to="/">
-              Home
-            </Nav.Link>
             <Nav.Link as={Link} to="/courses">
               Courses
             </Nav.Link>
