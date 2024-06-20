7eccf6d4c4c6cc00fd4b42a3f684d9a5b412ee47
Ajetech1 <jeleelajetunmobi@gmail.com>
2024-06-20 07:43:23 +0100

    Carousel features added to the herosection image

--- src/Components/HeroSection1.jsx
+++ src/Components/HeroSection1.jsx
@@ -1,6 +1,9 @@
 import React from "react";
-import { Container, Row, Col, Image } from "react-bootstrap";
-import Heroimage from "../assets/images/landingpage-hero.png";
+import { Container, Row, Col, Carousel } from "react-bootstrap";
+import Heroimage1 from "../assets/images/Hero-image1.png";
+import Heroimage2 from "../assets/images/Hero-image2.png";
+import Heroimage3 from "../assets/images/Hero-image3.png";
+import Heroimage4 from "../assets/images/Hero-image4.png";
 import "../assets/css/HeroSection.css";
 import PrimaryButton from "./PrimaryButton";
 
@@ -8,8 +11,37 @@ const HeroSection = () => {
   return (
     <Container fluid className="hero-section" style={{ padding: "80px 0" }}>
       <Row className="align-items-center">
-        <Col md={7}>
-          <Image src={Heroimage} fluid alt="Hero Image" />
+        <Col md={7} xs={12}>
+          <Carousel>
+            <Carousel.Item>
+              <img
+                className="d-block w-100"
+                src={Heroimage1}
+                alt="First slide"
+              />
+            </Carousel.Item>
+            <Carousel.Item>
+              <img
+                className="d-block w-100"
+                src={Heroimage2}
+                alt="Second slide"
+              />
+            </Carousel.Item>
+            <Carousel.Item>
+              <img
+                className="d-block w-100"
+                src={Heroimage3}
+                alt="Third slide"
+              />
+            </Carousel.Item>
+            <Carousel.Item>
+              <img
+                className="d-block w-100"
+                src={Heroimage4}
+                alt="Fourth slide"
+              />
+            </Carousel.Item>
+          </Carousel>
         </Col>
         <Col md={4} className="hero-section-text">
           <h2>Kickstart a career in Tech and become globally valuable</h2>
