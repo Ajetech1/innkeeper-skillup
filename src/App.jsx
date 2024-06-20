01cd4f066f353c0c9e18eccb0da36bcd5d845d7c
sarahannie <sarahndianekwu@gmail.com>
2024-06-20 01:00:00 +0100

    done with application page

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import React from "react";
 import "./App.css";
-import ApplicationForm from "./Pages/ApplicationForm/ApplicationForm";
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
 
 function App() {
   return (
     <>
-      <ApplicationForm />
+    <ToastContainer />
+      
     </>
   );
 }
