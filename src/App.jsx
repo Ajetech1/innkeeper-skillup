82d01ef9628d395a8a908cccf2be3f27a7ed18d8
fawwazmak <fawwazmakinde47@gmail.com>
2024-06-21 08:41:39 +0100

    aboutUs hero section

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import TopNavbar from "./Components/TopNavbar";
 import LandingPage from "./Pages/LandingPage/LandingPage";
 import Footer from "./Components/footer/Footer"
 // import CoursesPage from "./Pages/LandingPage/CoursesPage";
-// import AboutPage from "./Pages/AboutPage/AboutPage";
+import AboutPage from "./Pages/AboutPage/AboutPage";
 // import ContactPage from "./Pages/ContactPage/ContactPage";
 // import AccountPage from "./Pages/ApplicationForm/ApplicationForm";
 
@@ -16,10 +16,10 @@ function App() {
       <div className="main-content">
         <Routes>
           <Route path="/" element={<LandingPage />} />
-          {/* <Route path="/courses" element={<CoursesPage />} />
+          {/* <Route path="/courses" element={<CoursesPage />} /> */}
           <Route path="/about" element={<AboutPage />} />
-          <Route path="/contact" element={<ContactPage />} />
-          <Route path="/account" element={<AccountPage />} /> */}
+          {/* <Route path="/contact" element={<ContactPage />} /> */}
+          {/* <Route path="/account" element={<AccountPage />} /> */}
         </Routes>
       </div>
       <Footer />
---
+++ src/Pages/AboutPage/AboutPage.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import HeroSection from "../../Components/herosection/HeroSection";
+
+const AboutPage = () => {
+  return (
+    <div>
+      <HeroSection />
+    </div>
+  );
+};
+
+export default AboutPage;
