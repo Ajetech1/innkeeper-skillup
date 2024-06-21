4e1b862e330aa8636cbd92ca23849927d6ae60d6
MAKINDE FAWWAZ <126459939+fawwazmak@users.noreply.github.com>
2024-06-21 10:15:41 +0100

    Update TwoCards.jsx

--- src/Components/herosection/TwoCards.jsx
+++ src/Components/herosection/TwoCards.jsx
@@ -5,7 +5,7 @@ import iconTwo from "/cardIcon2.png"
 
 const TwoCards = () => {
   return (
-    <div className="d-flex gap-5 px-5">
+    <div className="d-flex flex-md-row flex-col  gap-5 px-5">
       <Card className="text-center" cardTitle={"Our Vision"} caption={"To be established as Africa’s leading talent development organization with a truly global alumnus."} cardIcon={iconOne} />
       
       <Card cardTitle={"Our Mission"} caption={"To provide upskilling for sought-after roles through global collaboration and diverse learning methods To empower individuals in Africa with employability, entrepreneurship, and leadership skills to become catalysts for change."} cardIcon={iconTwo} />
