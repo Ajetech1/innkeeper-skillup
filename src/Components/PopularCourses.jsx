666a1454967dd05c98957d71b05e8ee81e4fe00c
fawwazmak <fawwazmakinde47@gmail.com>
2024-06-24 10:46:53 +0100

    edited the courses section on the landing page

--- src/Components/PopularCourses.jsx
+++ src/Components/PopularCourses.jsx
@@ -1,62 +1,63 @@
+import React from "react";
 import Card from "react-bootstrap/Card";
 import Col from "react-bootstrap/Col";
-import Row from "react-bootstrap/Row";
 import CoursesData from "./CoursesData";
-import PrimaryButton from "./PrimaryButton";
+import { Button } from "react-bootstrap";
+import { CiSearch } from "react-icons/ci";
 
 function PopularCourses() {
   return (
-    <Row
-      style={{
-        paddingLeft: "50px",
-        paddingRight: "50px",
-        gap: "10px",
-      }}
-    >
+    <div style={{ paddingLeft: "50px", paddingRight: "50px" }}>
       <h1 className="text-center mb-5">Popular Courses</h1>
-      {CoursesData.map((course, index) => (
-        <>
-          <Col xs={12} md={4} lg={3} className="mx-3" key={index}>
-            <Card
-              bg={course.variant.toLowerCase()}
-              text={course.variant.toLowerCase() === "light" ? "dark" : "black"}
-              style={{ width: "auto", height: "auto" }}
-              className="mb-2"
-            >
-              <h6 style={{ marginLeft: "16px", margin: "30px" }}>
-                {course.price}
-                <span class="float-end">&gt;</span>
-              </h6>
-              <Card.Img variant="top" src={course.image} alt={course.title} />{" "}
-              {/* Added image */}
-              <Card.Body>
-                <Card.Title>{course.title}</Card.Title>
-                <Card.Text as="div" style={{ fontSize: "12px" }}>
-                  {course.description && Array.isArray(course.description) ? (
-                    <ul>
-                      {course.description.map((point, i) => (
-                        <li key={i}>{point}</li>
-                      ))}
-                    </ul>
-                  ) : (
-                    <p>No description available</p>
-                  )}
-                </Card.Text>
-              </Card.Body>
-            </Card>
-          </Col>
-          {index === CoursesData.length - 1 && (
-            <Col xs={12} className="text-center mb-5 mt-3">
-              <PrimaryButton
-                onClick={() => (window.location.href = "/all-courses")}
+      <div className="row">
+        {CoursesData.map((course, index) => (
+          <React.Fragment key={index}>
+            <div className="col-lg-4 col-md-6">
+              <Card
+                bg={course.variant.toLowerCase()}
+                text={
+                  course.variant.toLowerCase() === "light" ? "dark" : "black"
+                }
+                className="mb-2 px-3 py-2 text-sm-start text-center"
               >
-                See all courses
-              </PrimaryButton>
-            </Col>
-          )}
-        </>
-      ))}
-    </Row>
+                <h6 className="py-3">
+                  {course.price}
+                  <span className="float-end">&gt;</span>
+                </h6>
+                <Card.Img variant="top" src={course.image} alt={course.title} />{" "}
+                {/* Added image */}
+                <Card.Body>
+                  <Card.Title>{course.title}</Card.Title>
+                  <Card.Text as="div" style={{ fontSize: "12px" }}>
+                    {course.description && Array.isArray(course.description) ? (
+                      <ul>
+                        {course.description.map((point, i) => (
+                          <li key={i}>{point}</li>
+                        ))}
+                      </ul>
+                    ) : (
+                      <p>No description available</p>
+                    )}
+                  </Card.Text>
+                </Card.Body>
+              </Card>
+            </div>
+            {index === CoursesData.length - 1 && (
+              <Col xs={12} className="text-center mb-5 mt-3">
+                <Button onClick={() => (window.location.href = "/all-courses")}>
+                  See all courses
+                </Button>
+              </Col>
+            )}
+          </React.Fragment>
+        ))}
+      </div>
+
+      <div className="d-flex flex-sm-row flex-column w-50 mx-auto align-items-center justify-content-between bg-primary-subtle border p-3 rounded" style={{borderColor: "#a8b5c2"}}>
+        <input type="text" placeholder="Search for Courses" className="bg-transparent text-sm-start text-center" style={{border: "none"}} />
+        <CiSearch />
+      </div>
+    </div>
   );
 }
 
